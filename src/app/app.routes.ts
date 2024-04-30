afd1cde61a2f1451efd8509fb44531d8e14d30a7
hussein.hassan <hussein.hassan@tyson.com>
2024-04-30 22:00:53 +0300

    init commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
-export const routes: Routes = [];
+export const routes : Routes = [
+  {
+    path: '',
+    redirectTo: '/product-assets',
+    pathMatch: 'full'
+  },
+  {
+    path: 'product-assets',
+    loadComponent: () => import('./features/product-assets/product-assets.component').then(m => m.ProductAssetsComponent)
+  }
+];
