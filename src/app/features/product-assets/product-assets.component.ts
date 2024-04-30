4d6a40b2a2f46910c2672b927e2d8adeef25a1d3
hussein.hassan <hussein.hassan@tyson.com>
2024-05-01 01:15:25 +0300

    structure commit

--- src/app/features/product-assets/product-assets.component.ts
+++ src/app/features/product-assets/product-assets.component.ts
@@ -1,12 +1,56 @@
 import {Component} from '@angular/core';
+import {CommonModule} from "@angular/common";
+import {ConfirmDialogModule} from "primeng/confirmdialog";
+import {ToastModule} from "primeng/toast";
+import {ConfirmationService, MessageService} from "primeng/api";
 
 @Component({
   selector: 'app-product-assets',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ConfirmDialogModule, ToastModule],
+  providers: [ConfirmationService, MessageService],
   templateUrl: './product-assets.component.html',
   styleUrl: './product-assets.component.scss'
 })
 export class ProductAssetsComponent {
+  constructor(private confirmationService : ConfirmationService, private messageService : MessageService) {
+  }
 
+  confirm1(event : Event) {
+    this.confirmationService.confirm({
+      target: event.target as EventTarget,
+      message: 'Are you sure that you want to proceed?',
+      header: 'Confirmation',
+      icon: 'pi pi-exclamation-triangle',
+      acceptIcon: "none",
+      rejectIcon: "none",
+      rejectButtonStyleClass: "p-button-text",
+      accept: () => {
+        this.messageService.add({severity: 'info', summary: 'Confirmed', detail: 'You have accepted'});
+      },
+      reject: () => {
+        this.messageService.add({severity: 'error', summary: 'Rejected', detail: 'You have rejected', life: 3000});
+      }
+    });
+  }
+
+  confirm2(event : Event) {
+    this.confirmationService.confirm({
+      target: event.target as EventTarget,
+      message: 'Do you want to delete this record?',
+      header: 'Delete Confirmation',
+      icon: 'pi pi-info-circle',
+      acceptButtonStyleClass: "p-button-danger p-button-text",
+      rejectButtonStyleClass: "p-button-text p-button-text",
+      acceptIcon: "none",
+      rejectIcon: "none",
+
+      accept: () => {
+        this.messageService.add({severity: 'info', summary: 'Confirmed', detail: 'Record deleted'});
+      },
+      reject: () => {
+        this.messageService.add({severity: 'error', summary: 'Rejected', detail: 'You have rejected'});
+      }
+    });
+  }
 }
