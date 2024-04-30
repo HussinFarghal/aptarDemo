b64367f7320cf84d1599a4c1bd8ac537f1921ee0
hussein.hassan <hussein.hassan@tyson.com>
2024-04-30 22:01:11 +0300

    init commit

---
+++ src/app/features/product-assets/product-assets.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-product-assets',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-assets.component.html',
+  styleUrl: './product-assets.component.scss'
+})
+export class ProductAssetsComponent {
+
+}
