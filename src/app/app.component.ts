debd730c9244cfe1227bbb7986fe3e03706e178f
hussein.hassan <hussein.hassan@tyson.com>
2024-05-01 03:07:39 +0300

    structure commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import {Component, OnInit} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {CommonModule} from "@angular/common";
+import {HeaderComponent} from "./core/header/header.component";
+import {SidebarComponent} from "./core/sidebar/sidebar.component";
+import {FooterComponent} from "./core/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, HeaderComponent, SidebarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
