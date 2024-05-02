a95158bf85e3c0415c4767a4c7aa83e416b117ce
hussein.hassan <hussein.hassan@tyson.com>
2024-05-02 04:58:38 +0300

    structure commit

--- src/app/core/sidebar/sidebar.component.ts
+++ src/app/core/sidebar/sidebar.component.ts
@@ -1,12 +1,32 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {Sidebar, SidebarModule} from "primeng/sidebar";
+import {AvatarModule} from "primeng/avatar";
+import {ButtonModule} from "primeng/button";
+import {RippleModule} from "primeng/ripple";
+import {StyleClassModule} from "primeng/styleclass";
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [
+    AvatarModule,
+    ButtonModule,
+    RippleModule,
+    StyleClassModule,
+    SidebarModule,
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './sidebar.component.html',
   standalone: true,
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
+  @ViewChild('sidebarRef') sidebarRef! : Sidebar;
+  sidebarVisible : boolean = true;
+  protected readonly document = document;
 
+  closeCallback(e : Event) : void {
+    this.sidebarRef.close(e);
+  }
 }
