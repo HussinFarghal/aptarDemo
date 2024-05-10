a3393f7c6035f611bf9514ca6de9d7e07fec0e02
hussein.hassan <hussein.hassan@tyson.com>
2024-05-11 01:30:41 +0300

    products list

--- src/app/features/product-assets/components/products-list/products-list.component.ts
+++ src/app/features/product-assets/components/products-list/products-list.component.ts
@@ -3,7 +3,11 @@ import {of, Subscription, switchMap} from "rxjs";
 import {ProductAssetsService} from "../../product-assets.service";
 
 @Component({
-  selector: 'app-products-list', standalone: true, imports: [], templateUrl: './products-list.component.html', styleUrl: './products-list.component.scss'
+  selector: 'app-products-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './products-list.component.html',
+  styleUrl: './products-list.component.scss'
 })
 export class ProductsListComponent implements OnInit, OnDestroy {
   private productsSubscription : Subscription = new Subscription();
@@ -18,13 +22,11 @@ export class ProductsListComponent implements OnInit, OnDestroy {
   getProducts() {
 
     this.productsSubscription = this.productService.quickFiltersData$
-      .pipe(
-        // Use switchMap to switch to a new observable when quickFiltersData changes
+      .pipe(// Use switchMap to switch to a new observable when quickFiltersData changes
         switchMap(quickFiltersData => {
           if (quickFiltersData) {
             // Return the getProducts observable
-            return this.productService.getProductFamily(1, 10, 'lastUpdatedOn', 'desc',
-              quickFiltersData.product.value, quickFiltersData.assetName, quickFiltersData.finalCustomer.value);
+            return this.productService.getProductFamily(1, 10, 'lastUpdatedOn', 'desc', quickFiltersData.product.value, quickFiltersData.assetName, quickFiltersData.finalCustomer.value);
           }
           // Return an empty or default observable if there is no data
           else {
